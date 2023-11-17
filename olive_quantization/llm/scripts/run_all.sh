784640b0479733d9ace46220f2732fe5cb7ae7eb
zechariah0825 <zechariahrou@outlook.com>
2023-11-17 14:24:08 +0800

    修改

--- olive_quantization/llm/scripts/run_all.sh
+++ olive_quantization/llm/scripts/run_all.sh
@@ -1,6 +1,6 @@
-./scripts/clm_run.sh /data/huggingface/opt-6.7b/ wikitext wikitext-103-raw-v1 ant-int-flint 4 2 46666 outlier
+./scripts/clm_run.sh /home/gaozh/llama-7b wikitext wikitext-103-raw-v1 ant-int-flint 4 2 46666 outlier
 ./scripts/clm_run.sh facebook/opt-6.7b c4 realnewslike ant-int-flint 4 2 46666 outlier
 
-
-CUDA_VISIBLE_DEVICES=0 ./scripts/clm_run.sh facebook/opt-6.7b c4 realnewslike ant-int-flint 4 2 46667 outlier &
-CUDA_VISIBLE_DEVICES=0 ./scripts/clm_run.sh gpt2-xl c4 realnewslike ant-int-flint 4 8 46668 outlier &
+CUDA_VISIBLE_DEVICES=0 ./scripts/clm_run.sh OPT/opt-6.7b wikitext wikitext-103-raw-v1 ant-int-flint 8 2 46666 outlier &
+CUDA_VISIBLE_DEVICES=1 ./scripts/clm_run.sh LLAMA/llama-7b c4 realnewslike ant-int-flint 4 2 46666 outlier &
+#CUDA_VISIBLE_DEVICES=2 ./scripts/clm_run.sh gpt2-xl c4 realnewslike ant-int-flint 4 8 46668 outlier &
